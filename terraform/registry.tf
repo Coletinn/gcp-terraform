bbcd143631a07d961e57cf4821866fc2e3511c1f
Coletinn <gustavocoleto@hotmail.com>
2026-03-20 17:12:24 -0300

    added registry ensure

---
+++ terraform/registry.tf
@@ -0,0 +1,16 @@
+resource "google_artifact_registry_repository" "my_repo" {
+  location      = "us-central1"
+  repository_id = "flask-terraform"
+  description   = "Docker repository for Flask App"
+  format        = "DOCKER"
+
+  cleanup_policy_dry_run = false
+  cleanup_policies {
+    id     = "delete-old-images"
+    action = "DELETE"
+    condition {
+      tag_state    = "ANY"
+      older_than   = "1209600s"
+    }
+  }
+}

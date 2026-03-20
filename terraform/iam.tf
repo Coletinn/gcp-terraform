3886fc224c865f0642324acc85701ad30257c012
Coletinn <gustavocoleto@hotmail.com>
2026-03-20 15:43:16 -0300

    fixed iam tf file

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,13 +1,19 @@
 resource "google_project_iam_member" "cloudbuild_compute_admin" {
   project = var.project_id
   role    = "roles/compute.admin"
-  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+  member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "cloudbuild_storage_admin" {
   project = var.project_id
   role    = "roles/storage.admin"
-  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+  member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "cloudbuild_iam_user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 }
 
 data "google_project" "project" {}

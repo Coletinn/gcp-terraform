ec1c32bf87e5778c73a6fe43905c1db9dcacd646
Coletinn <gustavocoleto@hotmail.com>
2026-03-20 17:50:41 -0300

    added registry role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -16,4 +16,10 @@ resource "google_project_iam_member" "cloudbuild_iam_user" {
   member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 }
 
+resource "google_project_iam_member" "compute_registry_reader" {
+  project = "gcp-devops-490722"
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:488808357087-compute@developer.gserviceaccount.com"
+}
+
 data "google_project" "project" {}

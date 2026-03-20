e94822b4cd0920726b3d5b0e4d0ff4cb4006c95e
Coletinn <gustavocoleto@hotmail.com>
2026-03-20 17:36:09 -0300

    added import at registry

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -1,3 +1,8 @@
+import {
+  id = "projects/gcp-devops-490722/locations/us-central1/repositories/flask-terraform"
+  to = google_artifact_registry_repository.flask-terraform
+}
+
 resource "google_artifact_registry_repository" "flask-terraform" {
   location      = "us-central1"
   repository_id = "flask-terraform"
